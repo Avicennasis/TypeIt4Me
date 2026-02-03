d227a8c68c35da01dc7015ccdd1b98e2f5a13284
Avicennasis <Avicennasis@gmail.com>
2026-02-02 23:09:14 -0500

    Add LoggerBenchmark for logging performance tests

---
+++ TypeIt4Me.Performance/LoggerBenchmark.cs
@@ -0,0 +1,42 @@
+using BenchmarkDotNet.Attributes;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace TypeIt4Me.Performance
+{
+    [MemoryDiagnoser]
+    public class LoggerBenchmark
+    {
+        private string _logPath;
+        private string _content;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _logPath = Path.Combine(Path.GetTempPath(), "bench_error.log");
+            _content = new string('a', 1000); // 1KB log
+        }
+
+        [GlobalCleanup]
+        public void Cleanup()
+        {
+            if (File.Exists(_logPath))
+            {
+                File.Delete(_logPath);
+            }
+        }
+
+        [Benchmark(Baseline = true)]
+        public void SyncLog()
+        {
+            File.AppendAllText(_logPath, _content);
+        }
+
+        [Benchmark]
+        public async Task AsyncLog()
+        {
+            await File.AppendAllTextAsync(_logPath, _content);
+        }
+    }
+}
