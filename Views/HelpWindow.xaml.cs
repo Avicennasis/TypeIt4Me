da8eebde2bac54684b432eb4e17c83a65a89e24a
Léon Simmons <Avicennasis@gmail.com>
2026-06-19 03:22:44 -0400

    security: use HTTPS for external URLs (#36)
    Upgrade the GitHub project link (HelpWindow code-behind + displayed
    text) and the Apache License URL in LICENSE from http to https to avoid
    cleartext redirects / MITM on outbound links.
    
    Verified on the Windows build VM: build clean, 27/27 tests pass.

--- Views/HelpWindow.xaml.cs
+++ Views/HelpWindow.xaml.cs
@@ -13,7 +13,7 @@ namespace TypeIt4Me.Views
 
         private void Link_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            const string url = "http://github.com/avicennasis/TypeIt4Me";
+            const string url = "https://github.com/avicennasis/TypeIt4Me";
 
             if (Uri.TryCreate(url, UriKind.Absolute, out var uriResult) &&
                 (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps))
